ba548d10739df2c25c7794034ab3bdeebf9c1391
jayparekh4801 <parekhjay4801@gmail.com>
2021-04-01 17:06:17 +0530

    progress in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 class PreProcessing :
     
@@ -14,13 +16,27 @@ class PreProcessing :
         self.featureMetrics = self.dataset.iloc[ : , : -1].values
         self.resultMetrics = self.dataset.iloc[ : , -1].values
     
-    def handleNulls(self) :
-        self.imputer = SimpleImputer(missing_values="np.nan", strategy="mean")
+    def handleNulls(self, missing = "np.nan", strategy = "mean") :
+        self.imputer = SimpleImputer(missing_values=missing, strategy=strategy)
         self.imputer.fit_transform(self.featureMetrics)
     
-    def doSplit(self) :
-        self.featureMetrics_train, self.featureMetrics_test, self.resultMetrics_train, self.resultMetrics_test = train_test_split(self.featureMetrics, self.resultMetrics, test_size=0.2, random_state=0)
+    def doSplit(self, test = 0.2, state = 0) :
+        self.featureMetrics_train, self.featureMetrics_test, self.resultMetrics_train, self.resultMetrics_test = train_test_split(self.featureMetrics, self.resultMetrics, test_size=test, random_state=state)
 
-    def doFeatureScaling(self) :
-        scalar = StandardScaler()
+    def doFeatureScaling(self, method = "StandardScaler") :
+        if(method == "StandardScaler") :
+            scaler = StandardScaler()
+            self.featureMetrics_train = scaler.fit_transform(self.featureMetrics_train)
+            self.featureMetrics_test = scaler.transform(self.featureMetrics_test)
+        
+        else :
+            scaler = Normalizer()
+            self.featureMetrics_train = scaler.fit_transform(self.featureMetrics_train)
+            self.featureMetrics_test = scaler.transform(self.featureMetrics_test)
+
+    def handleCategoricalData(self, encoding = "onehotencoding", label = 0, remain = "passthrough") :
+        if(encoding == "onehotencoding" ) :
+            ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [label])], remainder=remain)
+            self.featureMetrics_train = ct.fit_transform(self.featureMetrics_train)
+            
         
